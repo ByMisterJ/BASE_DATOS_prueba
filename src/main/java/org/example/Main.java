6e8f7d84474e0a3d65a686919f6b8565fdff7d28
daw2 <josearanda9000@gmail.com>
2024-01-08 12:44:36 +0100

    commit sin acabar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,20 @@ import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
-        String sentenciaSQL = "Select * FROM personas";
-        try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost");
+        String sentenciaSQL = "Select * FROM actores";
+        String usuario="postgres";
+        String contraseña="postgres";
+        try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi base 01",usuario,contraseña);
             PreparedStatement sentencia= con.prepareStatement(sentenciaSQL)){
             ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()){
+                Integer id= resultados.getInt("id");
+                String nombre= resultados.getString("nombre");
+                Integer edad= resultados.getInt("edad");
+                System.out.println("id: "+id+" nombre: " +nombre+" edad: " +edad);
+            }
+
         }catch (Exception e){
             throw new RuntimeException(e);
         }
