99b98a92ad8632075f2a774c4b31c09b793890bb
daw2 <josearanda9000@gmail.com>
2024-01-10 12:38:13 +0100

    commit de la comprobacion de pilotos españoles y la implementacion de la base de datos f12006

---
+++ src/main/java/org/example/F1/F12006.java
@@ -0,0 +1,38 @@
+package org.example.F1;
+
+import java.sql.*;
+
+public class F12006 {
+    public static void main(String[] args) {
+        String urlConexion = "jdbc:postgresql://localhost:5432/F12006";
+        String usuario = "postgres";
+        String password = "postgres";
+
+//      driverid, code, forename, surname, dob, nationality, constructorid, url
+        String sentenciaSQL = "Select * FROM drivers";
+
+        try (Connection con = DriverManager.getConnection(urlConexion, usuario, password);
+            PreparedStatement sentencia= con.prepareStatement(sentenciaSQL)){
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()){
+                String nation = resultados.getString("nationality");
+                if (nation.equalsIgnoreCase("Spanish")) {
+
+                    Integer driverid = resultados.getInt("driverid");
+                    String code = resultados.getString("code");
+                    String forename = resultados.getString("forename");
+                    String surname = resultados.getString("surname");
+                    String dob = resultados.getString("dob");
+                    String nationality = resultados.getString("nationality");
+                    Integer constructorid = resultados.getInt("constructorid");
+                    String url = resultados.getString("url");
+
+                    System.out.println("driverid: "+driverid+" code: "+code+" forename: "+forename+" surname: "+surname+" dob: "+dob+" nationality: "+nationality+" constructorid: "+constructorid+" url: "+url);
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
+        }
+    }
+}
