04ff0e3a3d7979bc83ace6fee95298ceddd72b1f
daw2 <josearanda9000@gmail.com>
2024-01-12 10:02:39 +0100

    commit acabado de delete

--- src/main/java/org/example/Delete.java
+++ src/main/java/org/example/Delete.java
@@ -1,38 +1,32 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Delete {
     public static void main(String args[]) throws SQLException, ClassNotFoundException {
         Connection c = null;
-        Statement stmt = null;
         String user="postgres";
         String password="postgres";
         try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/mi base01", user, password);
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi base01", user, password);
+            PreparedStatement st = connection.prepareStatement("DELETE FROM actores WHERE nombre = ?");
+            st.setString(1,"Paco");
+            st.executeUpdate();
         } catch (Exception e) {
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
         }
         try {
-            stmt = c.createStatement();
-            String sql = "DELETE FROM actores"
-                    + "WHERE nombre=Paco";
-            stmt.executeUpdate(sql);
+            PreparedStatement st = c.prepareStatement("DELETE FROM Table WHERE nombre = Paco ;");
+            st.executeUpdate();
 
-            stmt.close();
+            st.close();
             c.commit();
             c.setAutoCommit(true);
             c.close();
         } catch (SQLException e) {
-            c.rollback();
+
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
         }
