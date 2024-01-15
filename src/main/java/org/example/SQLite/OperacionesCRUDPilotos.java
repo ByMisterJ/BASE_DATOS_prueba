b8b904a1be2bb831634ee5aece2f82b8ca27fa70
daw2 <josearanda9000@gmail.com>
2024-01-15 13:13:35 +0100

    commit sin acabar de OperacionesCRUDPilotos, añadiendo Actualizar pilotos

--- src/main/java/org/example/SQLite/OperacionesCRUDPilotos.java
+++ src/main/java/org/example/SQLite/OperacionesCRUDPilotos.java
@@ -40,35 +40,61 @@ public class OperacionesCRUDPilotos {
             con.close();
         }catch(Exception e){
             System.out.println("no se ha encontrado a nadie con ese id");
-            }
+        }
         return piloto;
     }
 
     public ArrayList<Piloto> LeerPilotos(Connection con) throws SQLException {
         ArrayList<Piloto> ListaPilotos = new ArrayList<>();
 
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM drivers ORDER BY driverid;");
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM drivers ORDER BY driverid;");
 
-        // Iterar sobre los resultados y agregar los pilotos a la lista
-        while (rs.next()) {
-            int driverId = rs.getInt("driverid");
-            String code = rs.getString("code");
-            String forename = rs.getString("forename");
-            String surname = rs.getString("surname");
-            String dob = rs.getString("dob");
-            String nationality = rs.getString("nationality");
-            String url = rs.getString("url");
+            // Iterar sobre los resultados y agregar los pilotos a la lista
+            while (rs.next()) {
+                int driverId = rs.getInt("driverid");
+                String code = rs.getString("code");
+                String forename = rs.getString("forename");
+                String surname = rs.getString("surname");
+                String dob = rs.getString("dob");
+                String nationality = rs.getString("nationality");
+                String url = rs.getString("url");
 
-            Piloto piloto = new Piloto(driverId, code, forename, surname, dob, nationality, url);
-            ListaPilotos.add(piloto);
+                Piloto piloto = new Piloto(driverId, code, forename, surname, dob, nationality, url);
+                ListaPilotos.add(piloto);
+            }
+
+            // Cerrar recursos
+            rs.close();
+            stmt.close();
+            con.close();
+        }catch(Exception e){
+            System.out.println("no se ha encontrado a nadie con ese id");
         }
 
-        // Cerrar recursos
-        rs.close();
-        stmt.close();
-        con.close();
-
         return ListaPilotos;
+    }
+
+    public void ActualizarPiloto(Connection con, Piloto piloto) throws SQLException {
+        String query = "UPDATE drivers SET code=?, forename=?, surname=?, dob=?, nationality=?, url=? WHERE driverid=?";
+
+        try (PreparedStatement pstmt = con.prepareStatement(query)) {
+            pstmt.setString(1, piloto.getCode());
+            pstmt.setString(2, piloto.getForename());
+            pstmt.setString(3, piloto.getSurname());
+            pstmt.setString(4, piloto.getDob());
+            pstmt.setString(5, piloto.getNationality());
+            pstmt.setString(6, piloto.getUrl());
+            pstmt.setInt(7, piloto.getDriverid());
+
+            int rowsUpdated = pstmt.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println("Registro actualizado exitosamente");
+            } else {
+                System.out.println("No se encontró ningún registro para actualizar");
+            }
+        }
     }
 }
